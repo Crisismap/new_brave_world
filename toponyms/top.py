86fb3c824bea75df5d2e23549c7838b3a3a0355e
anchen <anna.pereverzeva@gmail.com>
2017-05-20 16:30:53 +0300

    toponyms - improved version

--- toponyms/top.py
+++ toponyms/top.py
@@ -38,7 +38,7 @@ def extract_toponyms(text):
 
     words = data.get_tokens(text)
     if iseng(text):
-        nn = data.neurons(words, 1, eng_word2vec_model, lang = 'eng')
+        nn = data.neurons(words, 2, eng_word2vec_model, lang = 'eng')
         pred = eng_engine.predict(nn)
     else:
         nn = data.neurons(words, 1, rus_word2vec_model)
