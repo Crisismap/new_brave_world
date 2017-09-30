04198d099ee10ca62250771aa53522c9514f12b4
Filipp Minlos <fminlos@fminlos-osx.local>
2017-09-30 23:53:32 +0300

    trying more standart things

---
+++ experiments/process.py
@@ -0,0 +1,55 @@
+import pdb
+import gensim
+import pandas as pd
+import numpy as np
+
+from argparse import ArgumentParser
+from nltk.tokenize import word_tokenize
+from nltk.stem.snowball import RussianStemmer
+from pandas import DataFrame
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans
+import scipy.cluster.hierarchy as hcluster
+from sklearn.cluster import DBSCAN
+from sklearn.preprocessing import StandardScaler
+
+def handle_args():
+    parser = ArgumentParser()
+    parser.add_argument("-f", "--fname", default = 'allnews.csv')
+    args = parser.parse_args()
+    return args
+
+def is_cyrillic(symbol): return ord(symbol)>128
+def cyr(some_string): return any(is_cyrillic(s) for s in some_string) and not "the" in some_string.lower()
+
+def make_df(fname = "/Users/fminlos/my_projects/Crisismap_-_all_news/Crisismap - all news.csv", n = 1000):
+    df = pd.read_csv(fname, sep= ";", encoding = "utf8")
+    df = df[df.type.isin(["ukraine",  "fires", "ecology", "floods"])]
+    df["rus"] = df.Description.fillna("").map(cyr)
+    df = df[df.rus == True]
+    df = df.ix[np.random.permutation(df.index)]
+    df = df[:n]
+    df["text"] = df.Title + df.Description
+    return df
+
+stemmer = RussianStemmer()
+analyzer = TfidfVectorizer().build_analyzer()
+def stemmed_words(doc):
+    return (stemmer.stem(w) for w in analyzer(doc))
+stem_vectorizer = TfidfVectorizer(analyzer=stemmed_words)
+
+args = handle_args()
+fname = args.fname
+df = make_df(fname, n = 300)
+
+tfidf_matrix  =  stem_vectorizer.fit_transform(df.text)
+
+from make_clusters import  cartesian_vectorize
+coords = np.column_stack(cartesian_vectorize(df.lat, df.long))
+
+result = np.hstack((tfidf_matrix.toarray(), coords))
+kmodel = KMeans()
+df["clusters"] = kmodel.fit_predict(result)
+
+print df.clusters
