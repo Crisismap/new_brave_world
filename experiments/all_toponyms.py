6c20a7a15de59f3dada0b34cd3bb3fd91cb9c2dc
anchen <anna.pereverzeva@gmail.com>
2017-11-25 13:08:03 +0300

    toponyms extracted

---
+++ experiments/all_toponyms.py
@@ -0,0 +1,18 @@
+#-*- coding: UTF-8 -*-
+import pandas as pd
+from toponyms.top import extract_toponyms
+
+
+def lotop(text):
+    ts = tuple(extract_toponyms(text))
+    return ts
+
+data = pd.read_csv('Crisismap - all news.csv', sep = ';', encoding = 'utf-8', usecols = ['Title', 'Description'])
+print data
+data.dropna(inplace = True)
+
+
+data['text'] = data.Title + ' ' + data.Description
+data['toponyms'] = data.text.apply(lambda x: lotop(x))
+
+data.to_csv('crisismap_with_toponyms.csv', sep = '\t', encoding = 'utf-8', columns = ['Title', 'Description', 'toponyms'])
