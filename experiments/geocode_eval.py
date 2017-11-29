6c90d734a3e39b9bf542fdfde5391b1c64c76436
anchen <anna.pereverzeva@gmail.com>
2017-11-29 16:41:46 +0300

    bugfix

--- experiments/geocode_eval.py
+++ experiments/geocode_eval.py
@@ -12,20 +12,23 @@ import cPickle as pickle
 
 def handle_args():
     parser = ArgumentParser()
-    parser.add_argument('-f', '--fname', default='Crisismap - all news.csv')
+    parser.add_argument('-f', '--fname', default='crisismap_with_toponyms.csv')
     parser.add_argument('-g', '--geocod', default='google')
-    parser.add_argument('-n', '--number', default=1000, type=int)
+    parser.add_argument('-n', '--number', default=10, type=int)
     parser.add_argument('--random', dest='random',
                         action='store_true', default=True)
+    parser.add_argument('--no-random', dest= 'random', action = 'store_false')
+    parser.set_defaults(random=True)
     args = parser.parse_args()
     return args
 
 
-def geolocate(toponyms, geocod, coordinates):
+def geolocate(s, geocod, coordinates):
     coder = getattr(geocoder, geocod)
     locations = []
+    toponyms = s.split(', ')
     for toponym in toponyms:
-
+        #print toponym
         if toponym in coordinates.keys():
             location = coordinates[toponym]
         else:
@@ -67,46 +70,49 @@ def long(point):
         return ''
 
 
-def lotop(text):
-    from ..toponyms.top import extract_toponyms
-    ts = tuple(extract_toponyms(text))
+#def lotop(text):
+#    from ..toponyms.top import extract_toponyms
+#    ts = tuple(extract_toponyms(text))
 
-    return ts
+    #return ts
 
 
 def main():
     args = handle_args()
-    fname = 'new_brave_world/experiments/' + args.fname
+    #fname = 'new_brave_world/experiments/' + args.fname
+    fname = args.fname
+
     geocod = args.geocod
     number = args.number
     random = args.random
 
-    files = os.listdir(os.path.join(os.curdir, 'new_brave_world/experiments'))
+    #files = os.listdir(os.path.join(os.curdir, 'new_brave_world/experiments'))
+    files = os.listdir(os.curdir)
     if 'coordinates' in files:
-        with open('new_brave_world/experiments/coordinates', 'r') as file:
+        with open('coordinates', 'r') as file:
             coordinates = pickle.load(file)
     else:
         coordinates = {}
 
-    df = pd.read_csv(fname, sep=';', encoding="utf8")
+    df = pd.read_csv(fname, sep='\t', encoding="utf8")
     if random:
         print 'permutation'
         df = df.ix[np.random.permutation(df.index)]
     df = df[:number]
 
     df["text"] = df.Title.map(unicode) + df.Description.map(unicode)
-    df['toponyms'] = df.text.apply(lotop)
+    #df['toponyms'] = df.text.apply(lotop)
     df['points'] = df.toponyms.apply(geolocate, args=(geocod,coordinates))
     df['point'] = df.points.apply(main_point)
     df['lat'] = df.point.apply(lat)
     df['long'] = df.point.apply(long)
 
     now = dt.datetime.now().isoformat().replace(":", "_")
-    result_name = "_".join([os.path.splitext(fname)[0], now, "result.csv"])
+    result_name = "_".join([now, "result.csv"])
     columns = ["toponyms", 'lat', 'long', 'Title', "Description", 'points']
     df[columns].to_csv(result_name, encoding="utf8", sep="\t", index=False)
 
-    with open('new_brave_world/experiments/coordinates', 'w') as file:
+    with open('coordinates', 'w') as file:
         pickle.dump(coordinates, file)
 
 
